ff1e8ca233a974fcf91a8448d84ecb698ada3cc6
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-05 09:35:46 -0500

    commit - Arista / .

---
+++ src/Arista.java
@@ -0,0 +1,11 @@
+public class Arista {
+    public int origen;
+    public int destino;
+    public double distancia;
+
+    public Arista(int origen, int destino, double distancia) {
+        this.origen = origen;
+        this.destino = destino;
+        this.distancia = distancia;
+    }
+}   

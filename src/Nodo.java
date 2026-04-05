5b8e4539850625fbcfefb99c708c6b55b286ff93
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-05 09:34:14 -0500

    commit - Nodo .

---
+++ src/Nodo.java
@@ -0,0 +1,9 @@
+public class Nodo {
+    public int id;
+    public String nombre;
+
+    public Nodo(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;   // constructor .
+    }
+}

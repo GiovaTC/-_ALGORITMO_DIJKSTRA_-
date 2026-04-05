6ea1b0705af691693f17a94d848da00d42c771ab
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-05 10:28:40 -0500

    commit - CaminoLargo / .

---
+++ src/CaminoLargo.java
@@ -0,0 +1,23 @@
+ public class CaminoLargo {
+/*
+    public static void dfs(int nodo, Map<Integer, List<Arista>> grafo,
+                           Set<Integer> visitados, double distanciaActual,
+                           double[] maxDistancia) {
+
+        visitados.add(nodo);
+
+        maxDistancia[0] = Math.max(maxDistancia[0], distanciaActual);
+
+        if (grafo.containsKey(nodo)) {
+            for (Arista a : grafo.get(nodo)) {
+                if (!visitados.contains(a.destino)) {
+                    dfs(a.destino, grafo, visitados,
+                            distanciaActual + a.distancia, maxDistancia);
+                }
+            }
+        }
+
+        visitados.remove(nodo);
+    }
+*/
+}
